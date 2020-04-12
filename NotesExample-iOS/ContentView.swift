02f5249bf5a6ef23af2252442cc45b999306e929
Juan Francisco Miranda Aguilar <jfmiranda75@hotmail.com>
2020-04-12 19:37:43 +0200

    Initial Commit

---
+++ NotesExample-iOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NotesExample-iOS
+//
+//  Created by Juan Francisco Miranda Aguilar on 12/04/2020.
+//  Copyright © 2020 Juan Francisco Miranda Aguilar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
